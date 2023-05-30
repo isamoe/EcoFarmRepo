cbdfd4c7c47f5c2047da9fc3a24c8bb82fd9021f
Hannah Hielscher <hh057@hdm-stuttgart.de>
2023-05-30 21:12:09 +0200

    Toggle Shop and Move Button

--- Assets/Scripts/Shop_Open.cs
+++ Assets/Scripts/Shop_Open.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class Shop_Open : MonoBehaviour
 {
     public GameObject Panel;
+    //public GameObject Button;
     public void OpenPanel()
     {
         if (Panel != null)
         {
             bool isActive = Panel.activeSelf;
-
+            //Console.WriteLine(isActive);
+            print(isActive);
             Panel.SetActive(!isActive);
+            print(isActive);
+            /*Button.transform.TransformPoint(-220, 0, 0);*/
+            if (isActive == false)
+            {
+                var shop_icon = GameObject.Find("Canvas").transform.Find("Button").GetComponent<RectTransform>();
+                var pos = shop_icon.localPosition;
+                shop_icon.localPosition = new Vector3(-80, pos.y, pos.z);
+            }
+            if (isActive == true)
+            {
+                var shop_icon = GameObject.Find("Canvas").transform.Find("Button").GetComponent<RectTransform>();
+                var pos = shop_icon.localPosition;
+                shop_icon.localPosition = new Vector3(-290, pos.y, pos.z);
+            }
         }
     }
 }
